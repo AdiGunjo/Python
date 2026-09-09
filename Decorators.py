2dff74625547bf10ce5c38040445759a32d7b8d8
Harkonnen <gunjotikaraditya@gmail.com>
2026-09-09 08:14:02 +0530

    4.0

---
+++ Decorators.py
@@ -0,0 +1,18 @@
+import time
+from functools import wraps
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} took {time.time() - start:.4f}s")
+        return result
+    return wrapper
+
+@timer
+def slow_square(n):
+    time.sleep(0.2)
+    return n * n
+
+print(slow_square(5))
